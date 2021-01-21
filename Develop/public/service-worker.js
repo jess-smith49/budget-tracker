c5420297b0f7794d076c80a8faec5fe8c71923ad
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-21 10:39:57 -0600

    install complete, working on activate and fetch

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -14,4 +14,27 @@ const FILES_TO_CACHE = [
     '/public/icons/icon-72x72.png',
     '/public/icons/icon-72x72.png',
     '/public/icons/icon-72x72.png'
-]
+];
+
+
+//install service worker
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            console.log('Pre-cache successful')
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+//activate
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+
+    )
+})
+
+//fetch requests
+self.addEventListener('fetch', function (e) {
+    
+})
